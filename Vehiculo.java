481db809669e402833d7658eaf2131795ba2a6d6
Rubén Compte <compte_rub@gva.es>
2014-05-17 02:31:15 +0200

    Vehiculo Constructor

--- Vehiculo.java
+++ Vehiculo.java
@@ -4,8 +4,13 @@ public class Vehiculo {
 	public String matricula; 
 
 	public static void main(String[] args) {
-		Vehiculo v = new Vehiculo();
+		Vehiculo v = new Vehiculo("1234-AAA");
 
+	}
+
+	public Vehiculo(String matricula) {
+		super();
+		this.matricula = matricula;
 	}
 
 }
