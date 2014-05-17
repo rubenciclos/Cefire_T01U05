ec02b2575960129e33a39d70227c057c614e2c83
Rubén Compte <compte_rub@gva.es>
2014-05-17 02:27:20 +0200

    Anyadimos Getters y Setters

--- TodoTerreno.java
+++ TodoTerreno.java
@@ -1,9 +1,36 @@
 
-public class TodoTerreno extends Vehiculo{
+public class TodoTerreno extends Vehiculo{	
 	public String marchaReductora;
 	
 	public String suspension;
 	public String neumaticos;
 	public String amortiguador;
+	
+	public String getMarchaReductora() {
+		return marchaReductora;
+	}
+	public void setMarchaReductora(String marchaReductora) {
+		this.marchaReductora = marchaReductora;
+	}
+	public String getSuspension() {
+		return suspension;
+	}
+	public void setSuspension(String suspension) {
+		this.suspension = suspension;
+	}
+	public String getNeumaticos() {
+		return neumaticos;
+	}
+	public void setNeumaticos(String neumaticos) {
+		this.neumaticos = neumaticos;
+	}
+	public String getAmortiguador() {
+		return amortiguador;
+	}
+	public void setAmortiguador(String amortiguador) {
+		this.amortiguador = amortiguador;
+	}
+	
 
+	
 }
